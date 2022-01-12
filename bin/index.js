6eeef3cf9a9ab2dad652300b48d2f095dde22f35
RedCrafter07 <github@redcrafter07.de>
2022-01-12 17:45:50 +0100

    Add repository and NPM pulish stuff

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk'),
+/* const chalk = require('chalk'),
 	fs = require('fs'),
 	commandLineArgs = require('command-line-args'),
 	axios = require('axios').default;
@@ -10,4 +10,8 @@ import { Config } from 'node-json-db/dist/lib/JsonDBConfig';
 
 let db = new JsonDB(new Config('config', true, false, '/'));
 
-const mainDefinitions = [ { name: 'command', defaultOption: true } ];
+const mainDefinitions = [ { name: 'command', defaultOption: true } ]; */
+
+console.log(
+	chalk.yellow("This project is not done yet. It's just a placeholder for now but will be developed in the future.")
+);
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const chalk = require('chalk');
 
-console.log(chalk.red("I don't think this is what you wanted to do..."));
+/* console.log(chalk.red("I don't think this is what you wanted to do..."));
 console.log(chalk.yellow('If you want to install the eztwitch-cli, you can do so by running:'));
 console.log(chalk.green('npm i -g eztwitch'));
 console.log(chalk.yellow('After your installation you can run eztwitch in the console.'));
+ */
+
+console.log(
+	chalk.yellow("This project is not done yet. It's just a placeholder for now but will be developed in the future.")
+);
