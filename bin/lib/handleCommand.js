a92573a6cb7ad2252512cf03839372ebe0358d6e
RedCrafter07 <github@redcrafter07.de>
2022-01-13 19:35:17 +0100

    New commands

--- bin/lib/handleCommand.js
+++ bin/lib/handleCommand.js
@@ -1,3 +1,6 @@
+const chalk = require('chalk'),
+	log = require('../../lib/logging').log;
+
 module.exports = async (options, argv, db) => {
 	switch (options.command) {
 		case 'config':
@@ -5,6 +8,29 @@ module.exports = async (options, argv, db) => {
 			break;
 		case 'token':
 			await require('./commands/fetchToken')(argv, db);
+			break;
+		case 'create':
+			await require('./commands/create.js')(argv, db);
+			break;
+		case 'validate':
+			{
+				let data = await db.getData('/');
+				let out = new log(chalk.green(`Validating token...`));
+				let res = await require('../../lib/validate')(data.token);
+				if (res.status == 200) {
+					out.clear().edit(chalk.green(`Token is valid. \nDebug: ${JSON.stringify(res.data)}`));
+				} else {
+					out
+						.clear()
+						.edit(chalk.red(`Error! Status: ${res.status} | ${res.statusText} | Info:\n${res.data}`));
+				}
+			}
+			break;
+		case undefined:
+			await require('./commands/none')(argv, db);
+			break;
+		default:
+			await require('./commands/default')(options, argv, db);
 			break;
 	}
 };
